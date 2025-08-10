0001e23a13bbb050c4b108a007db45601114c296
pc-aide <36453201+pc-aide@users.noreply.github.com>
2025-08-10 14:59:52 -0400

    Create add-to-cart.cy.js

---
+++ Labs/v14/Lab01 - Add Products in Array/add-to-cart.cy.js
@@ -0,0 +1,19 @@
+describe('Adding items to the cart', () => {
+  it('should add all products to the cart, if any', () => {
+    let products: HTMLElement[] = [];
+
+    cy.visit('http://localhost:4200');
+    cy.get('.product-card').then(($el) => {
+      products = $el.toArray();
+      // The entire IF block needs to be inside the 'then' part
+      if (products.length) {
+        cy.wrap(products).each(product => {
+          cy.wrap(product).within(() => cy.get('.add-to-cart').click());
+        });
+        cy.get('a[href="/cart"] span').should('have.text', `Cart (${products.length})`);
+      } else {
+        cy.get('.product-card').should('have.length', 0);
+      }
+    });
+  });
+});
