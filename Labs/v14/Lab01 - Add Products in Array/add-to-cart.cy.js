5833fb3b283a0f8be320923ae95948c584ff0915
pc-aide <36453201+pc-aide@users.noreply.github.com>
2025-08-17 17:08:03 -0400

    Update add-to-cart.cy.js

--- Labs/v14/Lab01 - Add Products in Array/add-to-cart.cy.js
+++ Labs/v14/Lab01 - Add Products in Array/add-to-cart.cy.js
@@ -1,20 +1,21 @@
-describe('Adding items to the cart', () => {
-  it('should add all products to the cart, if any', () => {
-    // var mutable
-    let products: HTMLElement[] = [];
+describe('add product in Array', () => {
+    it('add products in Array', () => {
+        // var mutable
+        let products = [];
 
-    cy.visit('http://localhost:4200');
-    cy.get('.product-card').then(($el) => {
-      products = $el.toArray();
-      // The entire IF block needs to be inside the 'then' part
-      if (products.length) {
-        cy.wrap(products).each(product => {
-          cy.wrap(product).within(() => cy.get('.add-to-cart').click());
+        cy.visit('http://192.168.1.28:4200');
+        // 
+        cy.get('.product-card').then(($el) => {
+            products = $el.toArray();
+            // the entire if block needs to be inside the 'then' part
+            if (products.length) {
+                cy.wrap(products).each(product => {
+                    cy.wrap(product).within(() => cy.get('.add-to-cart').click());
+                });
+                cy.get('a[href="/cart"] span').should('have.text', `Cart (${products.length})`);
+            } else {
+                cy.get('.product-card').should('have.length', 0);
+            }
         });
-        cy.get('a[href="/cart"] span').should('have.text', `Cart (${products.length})`);
-      } else {
-        cy.get('.product-card').should('have.length', 0);
-      }
     });
-  });
 });
