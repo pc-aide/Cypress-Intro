5adb81995d866c79d9edcde111adcf894e6df3b0
pc-aide <36453201+pc-aide@users.noreply.github.com>
2025-08-17 14:36:00 -0400

    Update add-to-cart.cy.js

--- Labs/v14/Lab01 - Add Products in Array/add-to-cart.cy.js
+++ Labs/v14/Lab01 - Add Products in Array/add-to-cart.cy.js
@@ -1,5 +1,6 @@
 describe('Adding items to the cart', () => {
   it('should add all products to the cart, if any', () => {
+    // var mutable
     let products: HTMLElement[] = [];
 
     cy.visit('http://localhost:4200');
