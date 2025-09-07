e785b0dba41a9f8fad92f0131b23950c86dc0488
pc-aide <36453201+pc-aide@users.noreply.github.com>
2025-09-07 19:16:30 -0400

    Update rds.cy.js

--- cypress/e2e/rds.cy.js
+++ cypress/e2e/rds.cy.js
@@ -1,5 +1,9 @@
 describe('Visit RDS', () => {
   it('should visit rds.ca and take a screenshot', () => {
+    cy.on('uncaught:exception', (err, runnable) => {
+      // ignore all uncaught exceptions
+      return false;
+    });
     cy.visit('https://rds.ca');
     cy.screenshot('rds-homepage');
   });
